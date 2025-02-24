39974ff705f1ea3630aa839f375453f996fb8afc
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2025-02-24 13:57:33 +0100

    change fs to  writeContacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,12 +1,8 @@
-import fs from 'node:fs/promises';
-import { PATH_DB } from '../constants/contacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
 export const removeAllContacts = async () => {
-  try {
-    fs.writeFile(PATH_DB, '[]');
-  } catch (error) {
-    console.error('Failed to remove all contacts', error);
-    throw error;
-  }
+  const emptyContacts = [];
+  await writeContacts(emptyContacts);
 };
 
 removeAllContacts();
